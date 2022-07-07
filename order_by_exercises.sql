2a17ce8392003731583ee2e08bb16d0fdeb51298
Justin Welsh <justinwelsh33@gmail.com>
2022-07-07 16:35:37 -0500

    Completed Order By exercise

---
+++ order_by_exercises.sql
@@ -0,0 +1,21 @@
+USE employees;
+
+# Updates
+SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY first_name ASC;
+SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY first_name ASC, last_name ASC;
+SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY last_name ASC, first_name ASC;
+
+
+SELECT * FROM employees WHERE last_name LIKE 'E%' ORDER By emp_no ASC;
+# Reversed
+SELECT * FROM employees WHERE last_name LIKE 'E%' ORDER By emp_no DESC;
+
+SELECT * FROM employees WHERE last_name LIKE '%q%';
+
+# PART 2
+SELECT * FROM employees WHERE first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya';
+SELECT * FROM employees WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya') AND gender = 'M';
+
+SELECT * FROM employees WHERE last_name LIKE 'E%' OR last_name LIKE '%e';
+SELECT * FROM employees WHERE last_name LIKE 'E%e';
+SELECT * FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
