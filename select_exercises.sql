5e489ba1865057662e4d4adc12d7ed1b657d60e9
Justin Welsh <justinwelsh33@gmail.com>
2022-07-07 10:33:30 -0500

    update

--- select_exercises.sql
+++ select_exercises.sql
@@ -2,13 +2,6 @@ USE codeup_test_db;
 
 -- THE COMMAND TO RUN THIS FILE === mysql -u codeup_test_user -p < select_exercises.sql;
 
---         id INT UNSIGNED NOT NULL AUTO_INCREMENT,
---         artist VARCHAR(50),
---         name VARCHAR(100) NOT NULL,
---         release_date INT UNSIGNED NOT NULL,
---         sales FLOAT UNSIGNED NOT NULL,
---         genre VARCHAR(50),
---         PRIMARY KEY (id)
 
 SELECT name
     FROM albums
@@ -18,19 +11,19 @@ SELECT release_date
 FROM albums
 WHERE name = 'Sgt. Peppers Lonely Hearts Club Band';
 
--- SELECT genre
--- FROM albums
--- WHERE name = 'Nevermind';
-
--- SELECT name
--- FROM albums
--- WHERE release_date BETWEEN 1989 AND 2000;
-
--- SELECT name
--- FROM albums
--- WHERE sales < 20000000;
+SELECT genre
+FROM albums
+WHERE name = 'Nevermind';
 
 SELECT name
 FROM albums
-WHERE genre = 'Rock'; --Why do these query results not include albums with a genre of "Hard rock" or "Progressive rock"?
+WHERE release_date BETWEEN 1990 AND 1999;
+
+SELECT name
+FROM albums
+WHERE sales < 20000000;
+
+SELECT name
+FROM albums
+WHERE genre = 'Rock';
 
--- update_exercises.sql
+++ update_exercises.sql
@@ -1,21 +1,20 @@
 USE codeup_test_db;
 
--- SELECT name FROM albums;
--- UPDATE albums
--- SET sales = sales * 10;
--- SELECT sales FROM albums;
+SELECT * FROM albums;
+UPDATE albums
+SET sales = sales * 10;
+SELECT * FROM albums;
 
 
--- SELECT name FROM albums WHERE release_date < 1980;
--- UPDATE albums
--- SET release_date = release_date - 100;
--- WHERE release_date <= 1980;
--- SELECT name FROM albums WHERE release_date < 1900;
+SELECT * FROM albums WHERE release_date < 1980;
+UPDATE albums
+SET release_date = release_date - 100;
+WHERE release_date < 1980;
+SELECT * FROM albums WHERE release_date < 1980;
 
 
-
--- SELECT name FROM albums WHERE artist = 'Michael Jackson';
+SELECT * FROM albums WHERE artist = 'Michael Jackson';
 UPDATE albums
 SET artist = 'Peter Jackson'
 WHERE artist = 'Michael Jackson';
-SELECT artist FROM albums;
+SELECT * FROM albums WHERE artist = 'Peter Jackson';
