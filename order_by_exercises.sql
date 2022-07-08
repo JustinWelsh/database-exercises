24863954dffd50ec5a04a3e208889fac10e90e39
Justin Welsh <justinwelsh33@gmail.com>
2022-07-08 09:20:41 -0500

    Update

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,14 +1,20 @@
 USE employees;
 
-# Updates
+#======================== Order_by_exercise Updates ========================
+
+#2
 SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY first_name ASC;
+#3
 SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY first_name ASC, last_name ASC;
+#4
 SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY last_name ASC, first_name ASC;
 
-
+#5
 SELECT * FROM employees WHERE last_name LIKE 'E%' ORDER By emp_no ASC;
-# Reversed
+
+#6 Reversed
 SELECT * FROM employees WHERE last_name LIKE 'E%' ORDER By emp_no DESC;
+#======================================================================
 
 SELECT * FROM employees WHERE last_name LIKE '%q%';
 
